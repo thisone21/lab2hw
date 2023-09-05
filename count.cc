230cb6438d08dffb7b7c8aaa9a4f9f1b2f813685
thisone21 <thisone21@g.skku.edu>
2023-09-05 20:07:56 +0900

    Modify count.cc

--- count.cc
+++ count.cc
@@ -3,4 +3,12 @@
 void count_number() {
     // lab2 //
     // do it yourself //
+    int i;
+    for (int i=0;i<10;i++)
+    {
+        std::cout << i << std::endl;
+    }
+    std::cout << "2020312607";
+
+    return 0;
 }
